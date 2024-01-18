2501e3d9c0779142757d37e4768f2a8dd7c93e48
ikezaki <izumiikezaki@gmail.com>
2024-01-19 04:29:53 +0900

    テスト確認用ページ整備

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import Header from './Header.vue'
+import SideBlock from './Sideblock.vue'
 
 createApp(Header).mount('#app-header')
+createApp(SideBlock).mount('#app-sideblock')
--- src/sideblock.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import Sideblock from './Sideblock.vue'
-
-createApp(Sideblock).mount('#app-sideblock')
